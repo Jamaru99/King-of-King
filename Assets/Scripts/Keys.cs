78bab4a1410bdc832f42f146e50e0e1230c558fe
Matheus Oliveira <matheus.euf@gmail.com>
2022-04-21 21:17:16 -0300

    player movement + rotation

---
+++ Assets/Scripts/Keys.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keys
+{
+  public const string KeyAnimatorIsWalking = "isWalking";
+  public const string KeyAxisHorizontal = "Horizontal";
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using static Keys;
+
 public class Player : MonoBehaviour
 {
   private Animator animator;
 
   private float speed = 5f;
+  private bool isFacingLeft = false;
 
   void Start()
   {
@@ -16,14 +19,31 @@ public class Player : MonoBehaviour
   void FixedUpdate()
   {
     HandleMovement();
+    HandleRotation();
   }
 
   void HandleMovement()
   {
-    float horizontal = Input.GetAxis("Horizontal");
+    float horizontal = Input.GetAxis(KeyAxisHorizontal);
     bool isWalking = horizontal != 0;
     float amtToMove = horizontal * speed * Time.deltaTime;
-    transform.Translate(Vector3.right * amtToMove);
-    animator.SetBool("isWalking", isWalking);
+
+    transform.position += Vector3.right * amtToMove;
+
+    animator.SetBool(KeyAnimatorIsWalking, isWalking);
+  }
+
+  void HandleRotation()
+  {
+    if (Input.GetKey(KeyCode.LeftArrow) && !isFacingLeft)
+    {
+      isFacingLeft = true;
+      transform.Rotate(0, 180, 0);
+    }
+    if (Input.GetKey(KeyCode.RightArrow) && isFacingLeft)
+    {
+      isFacingLeft = false;
+      transform.Rotate(0, 180, 0);
+    }
   }
 }
